9b83c2fd2d947877170b83d7d7175e5192b705e6
John <messenger29.apps@gmail.com>
2015-08-03 13:27:49 -0700

    refactored and optimized code
    - three functional objects: domUpdate (for DOM manipulations),
    productobj (improved template function, calling it once instead each
    time), salesModel (for retrieving data only)

--- script.js
+++ script.js
@@ -1,37 +1,43 @@
-
-function domobj(){
+//model to retrieve data
+function salesModel(){
   var self        =this;
-  self.products   = [];
-
   //given url, get json data, parse each sales object into productobj and push into products variable
-  self.getproducts = function(url){
+  //after parsing, run callback or return products
+  self.getproducts = function(url,callback){
+    var products = [];
     $.getJSON(url, function(response){
         for(i=0; i<response.sales.length ; i++){
-          self.products.push( new productobj(response.sales[i], i)  );
+          products.push( new productobj(response.sales[i], i)  );
         }
+    }).done(function(){
+      if(typeof callback === 'function'){
+        callback(products);
+      }
+      else{
+        return products;
+      }
     });
   }
-    
-  //goes through each product and call updatehtml to create html snippet for it
-  self.updateproducthtml = function(){
-    for( i=0; i< self.products.length ; i++){
-      self.products[i].updatehtml();
-    }
-  }
-  
-  self.updatedom = function(){
-    var i=0
-    thishtml='';
-    for( i=0; i< self.products.length ; i++){
-      thishtml += self.products[i].htmlview;
-    }
-    $("#loading").remove();
-    $("#content").append(thishtml)  //print to html
-  }
-  
 }
 
-//parse product object
+//all DOM manipulations
+function domUpdate(){
+  var self      =this;
+
+  //combined updateproducthtml and updatehtml
+  self.updateproducthtml = function(products){
+    products[0].getTemplate(function(template){
+      $('#loading').toggleClass('hidden');
+      thishtml='';
+      for( i=0; i< products.length ; i++){
+        $('#content').append(products[i].updatehtml(template));
+      }
+      $('#loading').toggleClass('hidden');
+    });
+  }
+}
+
+//product object
 function productobj(product, i){
   var self          = this;
   self.photo        = product.photos.medium_half
@@ -42,16 +48,29 @@ function productobj(product, i){
   self.htmlview     = ""
   self.index        = i
   self.custom_class = "col-md-4"
-  
+
   //create and fill in html snippet for this
-  self.updatehtml= function(){
+  self.updatehtml= function(template){
+      return template.replace('{image}', self.photo).replace('{title}', self.title).replace('{tagline}', self.tagline).replace('{description}', self.description).replace('{url}', self.url).replace('{custom_class}', self.custom_class);
+  }
+
+  //get template unique to products
+  //after getting template, run callback or return template
+  self.getTemplate= function(callback){
     $.get('product-template.html', function(template){
-      self.htmlview = template.replace('{image}', self.photo).replace('{title}', self.title).replace('{tagline}', self.tagline).replace('{description}', self.description).replace('{url}', self.url).replace('{custom_class}', self.custom_class);
+      //if there is a callback function
+      if(typeof callback === 'function'){
+        callback(template);
+      }
+      else{
+        return template;
+      }
+      
     });
   }
 }
 
-//remove product item
+//remove product item functionality
 $(document).on('click','.product-remove',function(){
   //fadeout transition for product
   $(this).parent().parent().fadeOut(300, function() {
@@ -60,7 +79,7 @@ $(document).on('click','.product-remove',function(){
   });
 });
 
-var page=new domobj();
-page.getproducts('data.json');
-setTimeout("console.log('building html');page.updateproducthtml();",100); //changed timeout value from 20 to 100 b/c getproducts taking longer
-setTimeout("page.updatedom()",200)  //changed timeout value from 50 to 200 to go last
+var sales = new salesModel();
+var page = new domUpdate();
+
+sales.getproducts('data.json',page.updateproducthtml);
